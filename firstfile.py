e3576959e228942d1c95244c6b1d18e0b6f0ead3
Omkar <omkarpatil0117@gmail.com>
2020-01-23 00:48:22 +0530

    update for learnt till tuples

--- firstfile.py
+++ firstfile.py
@@ -1,5 +1,14 @@
-from math import *
-userinput = input("Enter anything : ")
+from math import * # todo import this lib
+# color = input("Enter a Color : ")
+# celeb = input("Enter a Celebrity : ")
+# brand = input("Enter a Brand : ")
+#
+# print ("I would love to paint my ass in " + color + ". I just want to do " + celeb + " while wearing " + brand)
 
-print("User entered :" + userinput)
-print(sqrt(3.66))
+list1 = ["Burhan","Vivek", "omkar","Vaibhav","omkar"]
+list2 = [2, 3, 450, 226, 3649,1,58]
+list3 = list1
+list2.sort()
+list2.reverse()
+print(type(list3))
+print(list3)
